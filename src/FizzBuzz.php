ff37ed84a33e567dccb4fba80cbc05cdfdb3c244
Ivan Stepaniuk <me@istepaniuk.com>
2019-10-30 14:21:08 +0100

    it_returns_one_for_one

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -4,5 +4,8 @@ namespace FizzBuzz;
 
 final class FizzBuzz
 {
-
+    public function transform(int $input): string
+    {
+        return $input;
+    }
 }
--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -7,5 +7,10 @@ use FizzBuzz\FizzBuzz;
 
 final class FizzBuzzTest extends TestCase
 {
+    public function test_it_transforms_1_to_1()
+    {
+        $fizzBuzz = new FizzBuzz();
 
+        self::assertEquals("1", $fizzBuzz->transform(1));
+    }
 }
