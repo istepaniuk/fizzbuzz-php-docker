2f0c85261f2e97c2a19b95c03b625465e1d9860c
Ivan Stepaniuk <me@istepaniuk.com>
2019-10-30 14:19:40 +0100

    no tests, no implementation

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -7,12 +7,5 @@ use FizzBuzz\FizzBuzz;
 
 final class FizzBuzzTest extends TestCase
 {
-    public function test_that_it_does_not_work()
-    {
-        $rover = new FizzBuzz();
 
-        // something...
-
-        self::assertTrue(false);
-    }
 }
